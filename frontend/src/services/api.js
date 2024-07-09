b089c0d8b725d3bf2356fc023135fa06fe72b29f
Kartik Sharma <krtksharma7@gmail.com>
2024-07-10 03:54:45 +0530

    Update api.js

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_URL = 'http://localhost:5000/api/tasks';
+const API_URL = 'https://task-manager-api-kappa.vercel.app/api/tasks';
 
 export const getTasks = async (filters = {}) => {
     try {
