f277c3b8216219449eb0290dbb39c98105068b63
krtksharma <krtksharma7@gmail.com>
2024-07-10 03:37:24 +0530

    backend fixed

--- backend/server.js
+++ backend/server.js
@@ -1,4 +1,3 @@
-
 const express = require('express');
 const http = require('http');
 const mongoose = require('mongoose');
@@ -13,7 +12,7 @@ const app = express();
 const server = http.createServer(app);
 const io = require('socket.io')(server, {
   cors: {
-    origin: 'http://localhost:3000',  
+    origin: 'http://localhost:3000',
     methods: ['GET', 'POST'],
   },
 });
@@ -27,10 +26,9 @@ mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopol
 app.use(cors());
 app.use(express.json());
 
-// Use the socket middleware
 app.use(socketMiddleware);
 
 app.use('/api/tasks', taskRoutes);
 
 const PORT = process.env.PORT || 5000;
-server.listen(PORT, () => console.log(`Server running on port ${5000}`));
+server.listen(PORT, () => console.log(`Server running on port ${PORT}`));
